7952248af53252cba9051cd15ec3914c725ce533
Marvin Luchs <marvin.luchs@holisticon.de>
2022-08-24 07:31:05 +0200

    add typescript support

--- extension/cockpit-plugin/rollup.config.js
+++ extension/cockpit-plugin/rollup.config.js
@@ -1,15 +1,17 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import replace from "@rollup/plugin-replace";
 import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: "src/main/frontend/plugin.js",
+  input: "src/main/frontend/plugin.ts",
   output: {
     file: "target/classes/plugin-webapp/correlate-cockpit-plugin/app/plugin.js"
   },
   plugins: [
     resolve(),
+    typescript(),
     babel({
             babelHelpers: "runtime",
             skipPreflightCheck: true,
