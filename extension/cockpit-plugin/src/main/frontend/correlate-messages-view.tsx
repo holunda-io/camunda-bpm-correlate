d77e4e1e981f0ec69b63ddad468448271819f827
Marvin Luchs <marvin.luchs@holisticon.de>
2022-08-24 09:38:45 +0200

    refactor messages and configuration into separate hooks

--- extension/cockpit-plugin/src/main/frontend/correlate-messages-view.tsx
+++ extension/cockpit-plugin/src/main/frontend/correlate-messages-view.tsx
@@ -1,14 +1,14 @@
-import { default as React, useCallback, useEffect, useState } from 'react';
+import { default as React } from 'react';
 import CorrelateMessagesTable from './components/correlate-table';
-import { Configuration } from './lib/configuration';
-import { Message } from './lib/message';
+import { useMessages } from './lib/message';
 
 type CorrelateMessagesViewProps = {
   camundaRestPrefix: string;
 }
 
 function CorrelateMessagesView({ camundaRestPrefix }: CorrelateMessagesViewProps) {
-  const { opLog, reload } = useOpLog(camundaRestPrefix);
+  const { messages, reload } = useMessages(camundaRestPrefix);
+  // const { configuration } = useConfiguration(camundaRestPrefix);
 
   return (
     <div className="ctn-view cockpit-section-dashboard">
@@ -17,10 +17,10 @@ function CorrelateMessagesView({ camundaRestPrefix }: CorrelateMessagesViewProps
           <section className="col-xs-12 col-md-12">
             <div className="inner">
               <h1 className="section-title">Messages</h1>
-              {opLog ? (
+              {messages.length > 0 ? (
                 <CorrelateMessagesTable
                   camundaRestPrefix={camundaRestPrefix}
-                  messages={opLog.messages}
+                  messages={messages}
                   reload={reload}
                 />
               ) : (
@@ -32,59 +32,6 @@ function CorrelateMessagesView({ camundaRestPrefix }: CorrelateMessagesViewProps
       </div>
     </div>
   );
-}
-
-type OpLog = {
-  messages: Message[];
-  configuration: Configuration;
-};
-
-function useOpLog(camundaRestPrefix: string) {
-  const [opLog, setOpLog] = useState<OpLog | null>(null);
-
-  const loadOpLog = useCallback(async (parameters?: MessageParams) => {
-    setOpLog(await fetchMessagesAndConfig(camundaRestPrefix, parameters));
-  }, [camundaRestPrefix]);
-
-  useEffect(() => {
-    const parameters = { page: 0, size: 100 };
-    loadOpLog(parameters);
-  }, []);
-
-  return {
-    opLog,
-    reload: loadOpLog
-  };
-}
-
-type MessageParams = {
-  page: number;
-  size: number;
-};
-
-async function fetchMessagesAndConfig(camundaRestPrefix: string, parameters?: MessageParams): Promise<OpLog | null> {
-  if (!parameters) {
-    parameters = { page: 0, size: 100 };
-  }
-  try {
-    const [configurationRes, messagesRes] = await Promise.all(
-      [
-        fetch(
-          `${camundaRestPrefix}/configuration`
-        ),
-        fetch(
-          `${camundaRestPrefix}/messages?page=${parameters.page}&size=${parameters.size}`
-        )
-      ]
-    );
-    return {
-      messages: await messagesRes.json(),
-      configuration: await configurationRes.json()
-    };
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
 }
 
 export default CorrelateMessagesView;
--- extension/cockpit-plugin/src/main/frontend/lib/message.ts
+++ extension/cockpit-plugin/src/main/frontend/lib/message.ts
@@ -1,3 +1,5 @@
+import { useCallback, useEffect, useState } from "react";
+
 export type MessageStatus = 'IN_PROGRESS' | 'MAX_RETRIES_REACHED' | 'PAUSED' | 'RETRYING';
 
 export type LocalDateTimeString = string;
@@ -16,3 +18,38 @@ export type Message = {
   nextRetry: LocalDateTimeString | null;
   error: string | null;
 };
+
+export function useMessages(camundaRestPrefix: string) {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const loadMessages = useCallback(async (parameters?: MessageParams) => {
+    setMessages(await fetchMessages(camundaRestPrefix, parameters) ?? []);
+  }, [camundaRestPrefix]);
+
+  useEffect(() => {
+    loadMessages();
+  }, []);
+
+  return {
+    messages,
+    reload: loadMessages
+  };
+}
+
+type MessageParams = {
+  page: number;
+  size: number;
+};
+
+async function fetchMessages(
+  camundaRestPrefix: string,
+  parameters: MessageParams = { page: 0, size: 100 }
+): Promise<Message[] | null> {
+  try {
+    const response = await fetch(`${camundaRestPrefix}/messages?page=${parameters.page}&size=${parameters.size}`);
+    return response.json();
+  } catch (error) {
+    console.error(`Fetching messages failed`, error);
+    return null;
+  }
+}
